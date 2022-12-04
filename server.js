16d29506edd61664f456619e3de184170570e9e0
varun8100 <95116684+varun8100s@users.noreply.github.com>
2022-12-05 04:24:48 +0530

    logs ip address of clients

--- server.js
+++ server.js
@@ -3,11 +3,11 @@ console.log("Port selected is: " + port)
 const io = require('socket.io')(port)
 var allClients = []
 io.on('connection', socket => {
+    console.log("active: "+allClients.length)
     allClients.push(socket)     
     console.log("new user: " + socket.id)
-    console.log("active: "+allClients.length)
     io.emit('client-connected', `Connected (${allClients.length} online), send a message...`)
-
+    
     socket.on('disconnect', () => {
         console.log("DISCONNECTED ----------------- "+socket.id)
         var i = allClients.indexOf(socket)
@@ -15,13 +15,13 @@ io.on('connection', socket => {
         io.emit('client-connected', `Connected (${allClients.length} online), send a message...`)
         console.log("active: "+allClients.length)
     })
-
     
-
+    
+    
     socket.on('send-client-message', msg => {
         console.log(msg)
         socket.broadcast.emit('broadcast', msg)
     })
+    socket.on('client-ip', data=>console.log(data))
 })
-
 
