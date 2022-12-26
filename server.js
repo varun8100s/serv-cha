aa7d9a0eddedfccfa2c92e5f0bb5b2ac48266b26
varun8100 <95116684+varun8100s@users.noreply.github.com>
2022-12-26 16:37:08 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,19 +1,18 @@
 const port = process.env.PORT
 console.log("Port selected is: " + port)
 const io = require('socket.io')(port)
-var allClients = []
+
 io.on('connection', socket => {
-    console.log("active: "+allClients.length)
-    allClients.push(socket)     
+    console.log("active: "+io.clients().server.eio.clientsCount)
+         
     console.log("new user: " + socket.id,"ip address:",socket.handshake)
-    io.emit('client-connected', `Connected (${allClients.length} online), send a message...`)
+    io.emit('client-connected', `Connected (${io.clients().server.eio.clientsCount} online), send a message...`)
     
     socket.on('disconnect', () => {
         console.log("DISCONNECTED ----------------- "+socket.id)
-        var i = allClients.indexOf(socket)
-        allClients.splice(i, 1)
-        io.emit('client-connected', `Connected (${allClients.length} online), send a message...`)
-        console.log("active: "+allClients.length)
+        
+        io.emit('client-connected', `Connected (${io.clients().server.eio.clientsCount} online), send a message...`)
+        console.log("active: "+io.clients().server.eio.clientsCount)
     })
     
     
