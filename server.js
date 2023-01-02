94d036706beb101fd7b2725ac2d3acda10ff42a5
varun8100 <95116684+varun8100s@users.noreply.github.com>
2023-01-03 03:17:13 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,31 +1,30 @@
-const port = process.env.PORT
-console.log("Port selected is: " + port)
-const io = require('socket.io')(port)
+const port = process.env.PORT;
+console.log(`Port selected is: ${port}`);
+const io = require('socket.io')(port);
 
-io.on('connection', socket => {
-    console.log("active: "+io.clients().server.eio.clientsCount)
-         
-    console.log("new user: " + socket.id,"ip address:",socket.handshake)
-    io.emit('client-connected', `Connected (${io.clients().server.eio.clientsCount} online), send a message...`)
-    
-    socket.on('disconnect', () => {
-        console.log("DISCONNECTED ----------------- "+socket.id)
-        
-        io.emit('client-connected', `Connected (${io.clients().server.eio.clientsCount} online), send a message...`)
-        console.log("active: "+io.clients().server.eio.clientsCount)
-    })
-    
-    
-    
-    socket.on('send-client-message', msg => {
-        console.log(msg)
-        socket.broadcast.emit('broadcast', msg)
-    })
-    
-    socket.on('typing', who => {
-        socket.broadcast.emit('someone-typing',who)
-    })
-    
-    
-})
+io.on('connection', (socket) => {
+  console.log(`Active: ${io.clients().server.eio.clientsCount}`);
+  console.log(`New user: ${socket.id}, IP address: ${socket.handshake}`);
+  io.emit(
+    'client-connected',
+    `Connected (${io.clients().server.eio.clientsCount} online), send a message...`
+  );
 
+  socket.on('disconnect', () => {
+    console.log(`DISCONNECTED ----------------- ${socket.id}`);
+    io.emit(
+      'client-connected',
+      `Connected (${io.clients().server.eio.clientsCount} online), send a message...`
+    );
+    console.log(`Active: ${io.clients().server.eio.clientsCount}`);
+  });
+
+  socket.on('send-client-message', (msg) => {
+    console.log(msg);
+    socket.broadcast.emit('broadcast', msg);
+  });
+
+  socket.on('typing', (who) => {
+    socket.broadcast.emit('someone-typing', who);
+  });
+});
